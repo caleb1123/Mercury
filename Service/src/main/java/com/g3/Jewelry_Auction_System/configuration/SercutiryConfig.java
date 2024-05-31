13e7d89b99ff58f5d584f19a268f85cb5e6bccdb
NamPhan <nampnhse173502@fpt.edu.vn>
2024-05-31 16:23:18 +0700

    update jwt

--- Service/src/main/java/com/g3/Jewelry_Auction_System/configuration/SercutiryConfig.java
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/configuration/SercutiryConfig.java
@@ -1,20 +1,29 @@
 package com.g3.Jewelry_Auction_System.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SercutiryConfig {
     private final String [] PUBLIC_ENDPOINTS = {
-            "/accounts/**",
+//            "/accounts/**",
             "/auth/**"
     };
+
+    @Value("${app.jwt-secret}")
+    private String signerKey;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
@@ -23,7 +32,23 @@ public class SercutiryConfig {
                 .anyRequest()
                 .authenticated());
 
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
+    }
+
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(),"HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+
+
     }
 }
