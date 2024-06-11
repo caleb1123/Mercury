91a67a328464fdb5542660c07e5173756643f4dd
ngochan1301 <hanxinhh1301@gmail.com>
2024-06-11 15:35:09 +0700

    add Jewelry, Post, PostCategory api

--- Service/src/main/java/com/g3/Jewelry_Auction_System/service/impl/JewelryServiceImpl.java
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/service/impl/JewelryServiceImpl.java
@@ -6,11 +6,15 @@ import com.g3.Jewelry_Auction_System.entity.Jewelry;
 import com.g3.Jewelry_Auction_System.exception.AppException;
 import com.g3.Jewelry_Auction_System.exception.ErrorCode;
 import com.g3.Jewelry_Auction_System.payload.DTO.JewelryDTO;
+import com.g3.Jewelry_Auction_System.repository.JewelryCategoryRepository;
 import com.g3.Jewelry_Auction_System.repository.JewelryRepository;
 
 import com.g3.Jewelry_Auction_System.service.JewelryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JewelryServiceImpl implements JewelryService {
@@ -18,6 +22,8 @@ public class JewelryServiceImpl implements JewelryService {
     JewelryRepository   jewelryRepository;
     @Autowired
     JewelryConverter    jewelryConverter;
+    @Autowired
+    private JewelryCategoryRepository jewelryCategoryRepository;
 
     @Override
     public JewelryDTO addJewelry(JewelryDTO jewelryDTO) {
@@ -34,4 +40,40 @@ public class JewelryServiceImpl implements JewelryService {
         jewelry.setStatus(false);
         jewelryRepository.save(jewelry);
     }
+
+    @Override
+    public void updateJewelry(JewelryDTO jewelryDTO, int id) {
+        Optional<Jewelry> optionalJewelry = jewelryRepository.findById(id);
+        if (jewelryDTO.getJewelryName().isEmpty()
+                || jewelryDTO.getImage().isEmpty()
+                || jewelryDTO.getDescription().isEmpty()
+                || jewelryDTO.getCondition().isEmpty()
+                || jewelryDTO.getEstimate() < 0
+                || jewelryDTO.getStartingPrice() < 0) {
+            throw new AppException(ErrorCode.EMPTY_FIELD);
+        }else if (optionalJewelry.isPresent()) {
+            Jewelry jewelry = optionalJewelry.get();
+            jewelry.setJewelryName(jewelryDTO.getJewelryName());
+            jewelry.setDesigner(jewelryDTO.getDesigner());
+            jewelry.setGemstone(jewelryDTO.getGemstone());
+            jewelry.setImage(jewelryDTO.getImage());
+            jewelry.setDescription(jewelryDTO.getDescription());
+            jewelry.setCondition(jewelryDTO.getCondition());
+            jewelry.setEstimate(jewelryDTO.getEstimate());
+            jewelry.setStartingPrice(jewelryDTO.getStartingPrice());
+            jewelry.setStatus(jewelryDTO.getStatus());
+            jewelry.setJewelryCategory(jewelryCategoryRepository.getReferenceById(jewelryDTO.getJewelryCategoryId()));
+            jewelryRepository.save(jewelry);
+        } else{
+            throw new RuntimeException("Jewelry not found");
+        }
+    }
+
+    @Override
+    public List<JewelryDTO> getAllJewelry() {
+        List<Jewelry> jewelryList = jewelryRepository.findAll();
+        return jewelryConverter.convertToJewelryDTOList(jewelryList);
+    }
+
 }
+
--- Service/src/main/java/com/g3/Jewelry_Auction_System/service/impl/PostServiceImpl.java
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/service/impl/PostServiceImpl.java
@@ -1,6 +1,62 @@
 package com.g3.Jewelry_Auction_System.service.impl;
 
+import com.g3.Jewelry_Auction_System.converter.PostConverter;
+import com.g3.Jewelry_Auction_System.entity.Post;
+import com.g3.Jewelry_Auction_System.payload.DTO.PostDTO;
+import com.g3.Jewelry_Auction_System.repository.AccountRepository;
+import com.g3.Jewelry_Auction_System.repository.PostCategoryRepository;
+import com.g3.Jewelry_Auction_System.repository.PostRepository;
 import com.g3.Jewelry_Auction_System.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
 public class PostServiceImpl implements PostService {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private PostConverter postConverter;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private PostCategoryRepository postCategoryRepository;
+
+    @Override
+    public PostDTO createPost(PostDTO postDTO) {
+        Post post = postConverter.toEntity(postDTO);
+        postRepository.save(post);
+        return postDTO;
+    }
+
+    @Override
+    public void updatePost(PostDTO postDTO, int id) {
+        Optional<Post> opPost = postRepository.findById(id);
+        if (opPost.isPresent()) {
+            Post post = opPost.get();
+            post.setTitle(postDTO.getTitle());
+            post.setPostDate(LocalDate.now());
+            post.setContent(postDTO.getContent());
+            post.setStatus(postDTO.getStatus());
+            post.setAccount(accountRepository.getReferenceById((postDTO.getAccountId())));
+            post.setPostCategory(postCategoryRepository.getReferenceById(postDTO.getCategoryId()));
+            postRepository.save(post);
+        } else{
+            throw new RuntimeException("Post not found");
+        }
+    }
+
+    @Override
+    public void deletePost(int id){
+        Optional<Post> opPost = postRepository.findById(id);
+        if (opPost.isPresent()) {
+            Post post = opPost.get();
+            post.setStatus(false);
+            postRepository.save(post);
+        } else {
+            throw new RuntimeException("Post not found");
+        }
+    }
 }
