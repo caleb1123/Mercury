ad669353c485317f1a08c8ea1a4eb8c2e346cf97
ngochan1301 <hanxinhh1301@gmail.com>
2024-06-14 10:57:02 +0700

    update

--- Service/src/main/java/com/g3/Jewelry_Auction_System/controller/PostController.java
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/controller/PostController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-@PreAuthorize("hasRole('MANAGER')")
 @RestController
 @RequestMapping("posts")
 public class PostController {
