c4acac9616915bc014a7be945e915e481d391f90
EugeneDNG41 <duynguyen4100@gmail.com>
2024-06-29 20:32:17 +0700

    added postImage and jewelryImage

---
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/entity/JewelryImage.java
@@ -0,0 +1,28 @@
+package com.g3.Jewelry_Auction_System.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "JewelryImage")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JewelryImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int jewelryImageId;
+
+    @Column
+    private String jewelryImageURL;
+
+    @ManyToOne
+    @JoinColumn(name = "jewelryId")
+    private Jewelry jewelry;
+}
---
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/entity/PostImage.java
@@ -0,0 +1,26 @@
+package com.g3.Jewelry_Auction_System.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PostImage")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int postImageId;
+
+    @Column
+    private String postImageURL;
+
+    @ManyToOne
+    @JoinColumn(name = "postId")
+    private Post post;
+}
