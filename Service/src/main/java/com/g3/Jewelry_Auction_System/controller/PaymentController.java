0c16a7f4d768f5e9e7a9cd24a7120074726657c6
MinhTrung187 <nguyenminhtrung18072004@gmail.com>
2024-07-21 18:23:14 +0700

    delete post image fixed

--- Service/src/main/java/com/g3/Jewelry_Auction_System/controller/PaymentController.java
+++ Service/src/main/java/com/g3/Jewelry_Auction_System/controller/PaymentController.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3001")
 @RequestMapping("/payment")
 public class PaymentController {
     @Autowired
